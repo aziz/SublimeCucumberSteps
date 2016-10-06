50e607fdbd25f59ddee3a4771f1d96d713ab3181
Allen Bargi <allen.bargi@gmail.com>
2016-10-07 01:40:07 +0200

    initial idea

---
+++ behat_annotator.py
@@ -0,0 +1,51 @@
+import sublime_plugin
+import sublime
+from . import behat_step
+
+
+class BehatStepAnnotator(sublime_plugin.ViewEventListener):
+
+    @classmethod
+    def applies_to_primary_view_only(cls):
+        return True
+
+    @classmethod
+    def is_applicable(cls, settings):
+        syntax = settings.get('syntax')
+        # TODO: list of valid systaxes
+        if syntax.endswith('Behat-Features.tmLanguage'):
+            return True
+        return False
+
+    def is_valid_step(self, step):
+        for keyword in behat_step.BEHAT_KEYWORDS:
+            if step.startswith(keyword):
+                return True
+        return False
+
+    def on_navigate(self, url):
+        self.view.window().open_file(url, sublime.ENCODED_POSITION)
+
+    def on_selection_modified_async(self):
+        view = self.view
+        window = view.window()
+        cursor = view.sel()[0]
+        line_range = view.line(cursor)
+        line_content = view.substr(line_range).strip()
+        root_folder = window.extract_variables()['folder']
+        css = sublime.load_resource("Packages/BehatStep/html/ui.css")
+        html = sublime.load_resource("Packages/BehatStep/html/ui.html")
+
+        if self.is_valid_step(line_content):
+            matches = behat_step.CucumberStepFinder(line_content, root_folder).matches
+            region = sublime.Region(line_range.b, line_range.b)
+            view.erase_phantoms("behat")
+            if len(matches) == 1:
+                m = matches[0]
+                content = '<span class="label label-success"><a href="' + m[0] + ':' + m[1] + '">➜ Go to implementation</a></span>'
+                view.add_phantom("behat", region, html.format(css=css, content=content), sublime.LAYOUT_INLINE, self.on_navigate)
+            else:
+                content = '<span class="label label-error">✘ implementation not found</span>'
+                view.add_phantom("behat", region, html.format(css=css, content=content), sublime.LAYOUT_INLINE)
+        else:
+            view.erase_phantoms("behat")
---
+++ behat_step.py
@@ -0,0 +1,86 @@
+import sublime_plugin
+import sublime
+import re
+import codecs
+from os import path, walk
+from . import case_parse
+
+BEHAT_KEYWORDS = ["Given", "When", "Then", "And", "But"]
+TEST_FOLDER = 'tests/acceptance'
+DEBUG = True
+
+
+class CucumberStepFinder():
+    def __init__(self, line, root):
+        self.matches = []
+        self.step = self.remove_keywords(line)
+        # TODO: return if line does not start with a keyword
+        self.fn_name = self.prepare_fn_name(self.step)
+        self.search_path = self.get_search_path(root)
+        self.matches = self.find_implementation(self.fn_name, self.search_path)
+
+        if DEBUG:
+            print("FN_NAME: " + self.fn_name)
+            print("SEARCH_PATH: " + self.search_path)
+            print(self.matches)
+
+    def remove_keywords(self, line):
+        step = line
+        for keyword in BEHAT_KEYWORDS:
+            if line.startswith(keyword):
+                step = line.replace(keyword, '', 1)
+        return step.strip()
+
+    def prepare_fn_name(self, step):
+        # TODO: handle <> and : and tables
+        step = re.sub(r'"[^"]+"', '', step)
+        return ' ' + self.to_camel_case(step) + '('
+
+    def to_camel_case(self, text):
+        words, case, sep = case_parse.parseVariable(text, False, [])
+        words[0] = words[0].lower()
+        return ''.join(words)
+
+    def get_search_path(self, root):
+        return path.join(root, TEST_FOLDER)
+
+    def find_implementation(self, text, search_path):
+        matches = []
+        for root, dirs, files in walk(search_path):
+            for f in files:
+                index = 0
+                file_path = path.join(root, f)
+                for line in codecs.open(file_path, 'r', 'utf-8'):
+                    index = index + 1
+                    if re.search(re.escape(text), line):
+                        matches.append([file_path, str(index), f, line])
+        return matches
+
+
+class BehatFindImplementationCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        # TODO: return if not valid file
+        window = self.view.window()
+        self.root_folder = window.extract_variables()['folder']
+        self.line = self.line_content()
+        self.matches = CucumberStepFinder(self.line, self.root_folder).matches
+
+        if len(self.matches) == 1:
+            m = self.matches[0]
+            window.open_file(m[0] + ':' + m[1], sublime.ENCODED_POSITION)
+
+        if len(self.matches) > 1:
+            items = ([m[2] + ':' + m[1], m[3]] for m in self.matches)
+            window.show_quick_panel(list(items), self.on_file_selection)
+
+        if len(self.matches) == 0:
+            window.status_message('=> Step not implemented!')
+
+    def on_file_selection(self, row):
+        pass
+
+    def line_content(self):
+        view = self.view
+        cursor = view.sel()[0]
+        line_range = view.line(cursor)
+        return view.substr(line_range).strip()
